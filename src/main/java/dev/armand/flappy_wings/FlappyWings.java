c51519723dd79a6791aafac4e00b007b0b0db730
phantomeye1931 <armand.devries@outlook.com>
2026-06-20 20:18:52 +0200

    Fix server crash

--- src/main/java/dev/armand/flappy_wings/FlappyWings.java
+++ src/main/java/dev/armand/flappy_wings/FlappyWings.java
@@ -7,8 +7,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import org.slf4j.Logger;
@@ -19,16 +17,12 @@ public class FlappyWings {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public FlappyWings(IEventBus modEventBus, ModContainer modContainer) {
-        LOGGER.info("Flappy Wings initialized. Vanilla elytra flight disabled.");
-
         FlappyWingsSounds.register(modEventBus);
         FlappyWingsParticles.register(modEventBus);
         modEventBus.addListener(this::registerPackets);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
         modEventBus.addListener(Config::onLoad);
-
-        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     private void registerPackets(final RegisterPayloadHandlersEvent event) {
--- src/main/java/dev/armand/flappy_wings/FlappyWingsClient.java
+++ src/main/java/dev/armand/flappy_wings/FlappyWingsClient.java
@@ -1,12 +1,20 @@
 package dev.armand.flappy_wings;
 
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @EventBusSubscriber(modid = FlappyWings.MOD_ID, value = Dist.CLIENT)
 public class FlappyWingsClient {
+
+    public FlappyWingsClient(IEventBus modEventBus, ModContainer modContainer) {
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
 
     @SubscribeEvent
     public static void registerProviders(RegisterParticleProvidersEvent event) {
